14ff4df89c12fedd5f05acb05c66723aca5e7a7c
Jhalak mandal <mandalshubham284@gmail.com>
2026-07-15 18:34:18 +0530

    Time: 0 ms (100%), Space: 30.5 MB (99.86%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -12,22 +12,13 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(root == NULL) return NULL;
+        if(root == NULL) return  NULL;
 
-        if(root->val == val) return root;
+        if(root->val == val ) return root;
 
-        if(root->val > val) 
-        {
-            return root->left = searchBST(root->left , val);
-        }
-        else{
-            return root->right = searchBST(root->right , val);
-        }
-
+        if(root->val > val ) return root->left = searchBST(root->left , val);
+        else if( root->val < val) return root->right = searchBST(root->right , val);
         
+        return root ;
     }
 };
-
-
-
-const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
