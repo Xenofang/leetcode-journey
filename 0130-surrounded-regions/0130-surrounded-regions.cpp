bff19393e0ed539cc3cc76c7e016ce0a2ea22ab8
Jhalak mandal <mandalshubham284@gmail.com>
2026-08-07 21:12:30 +0530

    Time: 0 ms (100%), Space: 13.8 MB (97.63%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    void dfs(int r , int c ,vector<vector<char>>& board )
+    {
+        int m = board.size();
+        int n = board[0].size();
+
+        // base case 
+        if( r < 0 || r == m ||
+            c < 0 || c == n ||
+            board[r][c] != 'O')
+            {
+                return ;
+            }
+
+        board[r][c] = 'T';
+
+        dfs(r-1, c, board); //top
+        dfs(r+1, c, board); // bottom
+        dfs(r, c-1, board); //left
+        dfs(r, c+1, board); //right 
+    }
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+
+        // for every edges if there is a O  we have  to channge it into temp T
+
+        for(int i= 0 ; i < m ; i++)
+        {
+            for(int j = 0 ; j < n ; j++)
+            {
+                if((i == 0 || i == m - 1 || j == 0 || j == n - 1)&& board[i][j] == 'O')
+                {
+                    dfs(i , j , board);
+                }
+            }
+        }
+
+
+        // convert all the remaning O into x 
+
+        for(int r = 0 ; r < m ; r++)
+        {
+            for(int c = 0 ; c < n ; c++)
+            {
+                if( r >= 0 && r < m &&
+                    c >= 0 &&  c < n &&
+                    board[r][c] == 'O')
+                {
+                    board[r][c] = 'X';
+                }
+
+                else if(r >= 0 && r < m &&
+                    c >= 0 &&  c < n &&
+                    board[r][c] == 'T')
+                {
+                    board[r][c] = 'O';
+                }
+            }
+        }
+
+    }
+};
